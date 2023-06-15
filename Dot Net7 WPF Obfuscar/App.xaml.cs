4ba360876658f1aed4998381ae63986a6b121431
Jaeyong Park <kkomzi@gmail.com>
2023-06-15 13:36:52 +0900

    초기 커밋.

---
+++ Dot Net7 WPF Obfuscar/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dot_Net7_WPF_Obfuscar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Dot Net7 WPF Obfuscar/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace Dot_Net7_WPF_Obfuscar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const string ConstStringValue = "난독화 문자열 상수";
+        public readonly string ReadonlyStringValue = "난독화 문자열 필드";
+        public static void RegisterStartUpProgram()
+        {
+            Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (key != null)
+            {
+                Assembly curAssembly = Assembly.GetExecutingAssembly();
+                var regName = curAssembly.GetName().Name;
+                ProcessModule? mainModule = Process.GetCurrentProcess().MainModule;
+                if (mainModule != null && !key.GetValueNames().Contains(regName))
+                {
+                    key.SetValue(regName, mainModule.FileName);
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
